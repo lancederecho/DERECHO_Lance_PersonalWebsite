b360872488a4845a7d1b9b81c5631419eac7ab83
lderecho12 <lderecho12@gmail.com>
2017-09-24 00:42:24 +0800

    2nd| Set styling, navigations, social media icons, title name and icon, partial background.

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
 <html>
-<title>LanceProfile</title>
-<head> 
-    <h1>LANCE GWAPO</h1>
-</head>
-<body>
+    
+    <head>
 
-yoohooo!!
-</body>
+        <title>Lance Derecho | Profile</title>
+        <link rel="shortcut icon" href="images/title.ico">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+
+    </head>
+    
+    <body >  
+
+        <header>
+            <div class="navbar">
+                <nav class="navbar">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Achievements</a></li>
+                    <li><a href="">Aspirations</a></li>
+                    <li><a href="">About Me</a></li>
+                </ul>
+                </nav>
+            </div>    
+        </header> 
+
+        <footer>
+            <div class="socialmedia">
+                <nav class="socialmedia">
+                <ul>
+                    <li>
+                        <a href="http://www.instagram.com/lance_derecho" target="_blank">
+                            <img class="socialpic" src="images/instagram.ico"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.facebook.com/lderecho12" target="_blank">
+                            <img class="socialpic" src="images/facebook.ico"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.twitter.com/lance_derecho" target="_blank">
+                            <img class="socialpic" src="images/twitter.ico"/>
+                        </a>
+                    </li>
+                </ul>
+                </nav>
+            </div>    
+            </footer> 
+
+    </body>
 
 </html>
