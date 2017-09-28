70e662a11aa5948549665074d2f1bc5af35f8bd8
lderecho12 <lderecho12@gmail.com>
2017-09-28 17:03:58 +0800

    37th| Adjusting code margin on aspirations.php file

--- aspirations.php
+++ aspirations.php
@@ -7,19 +7,19 @@
 
     <body>
 
-            <div class="navbaraspirations">   
-                <a class="navbarlogoaspirations" href="index.php">
-                    <img alt="logo" src="images/LanceLogo2.png"/>
-                </a> 
-                <nav class="navbaraspirations">
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="achievements.php">Achievements</a></li>
-                        <li><a href="aspirations.php">Aspirations</a></li>
-                        <li><a href="aboutme.php">About Me</a></li>
-                    </ul>
-                </nav>
-            </div>    
+        <div class="navbaraspirations">   
+            <a class="navbarlogoaspirations" href="index.php">
+                <img alt="logo" src="images/LanceLogo2.png"/>
+            </a> 
+            <nav class="navbaraspirations">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="achievements.php">Achievements</a></li>
+                    <li><a href="aspirations.php">Aspirations</a></li>
+                    <li><a href="aboutme.php">About Me</a></li>
+                </ul>
+            </nav>
+        </div>    
         
     </body>
 
