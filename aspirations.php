740ffead5b8ace36df9ab0e31857fbd8cd52c797
lderecho12 <lderecho12@gmail.com>
2017-09-28 17:00:42 +0800

    35th| Adding navbar links to aspirations page

--- aspirations.php
+++ aspirations.php
@@ -6,6 +6,17 @@
     </head>
 
     <body>
+
+            <div class="navbaraspirations">    
+                <nav class="navbaraspirations">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="achievements.php">Achievements</a></li>
+                        <li><a href="aspirations.php">Aspirations</a></li>
+                        <li><a href="aboutme.php">About Me</a></li>
+                    </ul>
+                </nav>
+            </div>    
         
     </body>
 
