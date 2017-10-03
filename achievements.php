69950f4d8b28cdbc572b2f866e2ec8023c3cdb5e
lderecho12 <lderecho12@gmail.com>
2017-10-03 21:58:24 +0800

    74th| Partially finished achievements page. Lacked some pics.

--- achievements.php
+++ achievements.php
@@ -63,7 +63,91 @@
         </div>
 
         <div class="achievementscontents">
-
+            <div class="firstachievements">
+                <div class="firstimages">
+                    <img src="images/achievements/first/1.jpg"/>
+                    <img src="images/achievements/first/2.jpg"/>
+                    <img src="images/achievements/first/3.jpg"/>
+                </div>
+                <pa>
+                    Finishing highschool: It is one of my achievements. I believe that it is true because this is the 
+                    time when almost all of us learns to depend on himself and learns some hints on the path
+                    that one may choose to live with.
+                </pa>
+            </div>
+            <div class="secondachievements">
+                <div class="secondimages">
+                    <img src="images/achievements/second/1.jpg"/>
+                    <img src="images/achievements/second/2.jpg"/>
+                    <img src="images/achievements/second/3.jpg"/>
+                </div>
+                <pa>
+                    Entering college: For me, I is a great achievements. I see it as a first step on becoming what I want
+                    to be and who I want to be in this life. 
+                </pa>
+            </div>
+            <div class="thirdachievements">
+                <div class="thirdimages">
+                    <img src="images/achievements/third/1.jpg"/>
+                    <img src="images/achievements/third/2.jpg"/>
+                    <img src="images/achievements/third/3.jpg"/>
+                </div>
+                <pa>
+                    Tennis Championship: Winning in sports championships is certainly a great achivement. It showcases 
+                    one's physical and intellectual capacities, and also it reflects one's hard work and discipline. 
+                    I can always remember those hard trainings and hard lessons.   
+                </pa>
+            </div>
+            <div class="fourthachievements">
+                <div class="fourthimages">
+                    <img src="images/achievements/fourth/1.jpg"/>
+                    <img src="images/achievements/fourth/2.jpg"/>
+                    <img src="images/achievements/fourth/3.jpg"/>
+                </div>
+                <pa>
+                    Learning to Code: Being able to write codes and programs is a very great thing for me because I have 
+                    a huge passion for it. Not only do I have passion for it, but I also have an enormous interest in it.
+                    I believe that it is a great privilage to know how to code.
+                </pa>
+            </div>
+            <div class="fifthachievements">
+                <div class="fifthimages">
+                    <img src="images/achievements/fifth/1.jpg"/>
+                    <img src="images/achievements/fifth/2.jpg"/>
+                    <img src="images/achievements/fifth/3.jpg"/>
+                </div>
+                <pa>
+                    Winning Band Contests: Learning to play a musical instrument, especially hearing your own sound when
+                    playing an alto saxophone is a very heart-warming and soul-refreshing experience. Winning a contest
+                    while playing one is a very gigantic achievement, and a very delighting one. 
+                </pa>
+            </div>
+            <div class="sixthachievements">
+                <div class="sixthimages">
+                    <img src="images/achievements/sixth/1.jpg"/>
+                    <img src="images/achievements/sixth/2.jpg"/>
+                    <img src="images/achievements/sixth/3.jpg"/>
+                </div>
+                <pa>
+                    Reading Lots of Books: Some years ago, I set a goal to read one book every month. I skipped a lot of months
+                    but still ended reading lots of books and learned lots of things from the books that I read. It takes 
+                    a lot discipline and self-control to read a book ,so because of that reason, I can consider it a great 
+                    achievement on my part. Not just finishing lots of books, but learning a lot of things.
+                </pa>
+            </div>
+            <div class="seventhachievements">
+                <div class="seventhimages">
+                    <img src="images/achievements/seventh/1.jpg"/>
+                    <img src="images/achievements/seventh/2.jpg"/>
+                    <img src="images/achievements/seventh/3.jpg"/>
+                </div>
+                <pa>
+                    Reading Lots of Books: Some years ago, I set a goal to read one book every month. I skipped a lot of months
+                    but still ended reading lots of books and learned lots of things from the books that I read. It takes 
+                    a lot discipline and self-control to read a book ,so because of that reason, I can consider it a great 
+                    achievement on my part. Not just finishing lots of books, but learning a lot of things.
+                </pa>
+            </div>
         </div>
 
     </body>
